928f68561d064081ae98672f6234bb313dd258a1
PawelSwiderski1 <pawel.m.swiderski@gmail.com>
2024-01-07 18:30:30 +0100

    Created a makeshift website using django to be able to handle counters (serving numbers, moving the queue along). Using it you can better test how the app works.

---
+++ DjangoAplikacjaKolejkowa/AplikacjaKolejkowa/consumers.py
@@ -0,0 +1,16 @@
+import asyncio
+import websockets
+
+
+class MyConsumer:
+    async def connect(self):
+        # Code to connect to your existing WebSocket server
+        pass
+
+    async def disconnect(self):
+        # Code to handle disconnection
+        pass
+
+    async def receive(self, text_data):
+        # Code to handle received data
+        pass
